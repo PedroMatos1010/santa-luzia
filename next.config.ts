a297e122d84b6f61fabcc966af03bbe3942b2916
PedroMatos1010 <pedromcmatos10@gmail.com>
2026-07-14 12:48:54 +0100

    Security breakpoint: fix local image optimization (private IP) + mobile improvements

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     '*.loca.lt'
   ],
   images: {
+    unoptimized: process.env.NODE_ENV === 'development',
     remotePatterns: [
       {
         protocol: 'https',
